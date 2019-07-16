2e45390617039a3787b7100b7cc09524e817452e
sajadhaja <sajadhaja90@gmail.com>
2019-07-16 21:05:20 +0200

    1 file modified

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,63 +1,20 @@
 import React, { useState, useContext, useEffect } from 'react';
 import Row from './Row';
-import { ThemeContext, LocaleContext } from './context';
 
 export default function Greeting() {
-  const name = useFormInput('Tom');
-  const surName = useFormInput('Mathew');
-  const theme = useContext(ThemeContext);
-  const locale = useContext(LocaleContext);
-  const width = useWindowWidth();
-  useDocumentTitle(name.value + ' ' + surName.value, name.value);
+ const [name, setName] = useState('Tom');
+
+ function handleNameChange(e) {
+   setName(e.target.value)
+ }
 
   return (
-    <section className={theme}>
+    <section >
       <Row label="Name">
-        <input
-          {...name} />
-      </Row>
-      <Row label="Surname">
-        <input
-          {...surName} />
-      </Row>
-      <Row label="Language">
-        {locale}
-      </Row>
-      <Row label="Width">
-        {width}
+        <input value = {name} onChange={handleNameChange} />
       </Row>
     </section>
   );
 
-}
-
-function useFormInput(intialValue) {
-  const [value, setValue] = useState(intialValue);
-  function handleChange(e) {
-    setValue(e.target.value);
-  }
-  return {
-    value: value,
-    onChange: handleChange
-  }
-}
-
-function useDocumentTitle(title, dep) {
-  useEffect(() => {
-    document.title = title;
-  }, [dep]);
-}
-
-function useWindowWidth() {
-  const [width, setWidth] = useState(window.innerWidth);
-  useEffect(() => {
-    const handleResize = () => setWidth(window.innerWidth);
-    window.addEventListener('resize', handleResize);
-    return () => {
-      console.log("cleanup listenrr");
-      window.removeEventListener('resize', handleResize);
-    };
-  });
-  return width;
 }
 

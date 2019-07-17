3285383263e38c9526ef90db089e57c14245a0ff
sajadhaja <sajadhaja90@gmail.com>
2019-07-17 16:14:16 +0200

    1 file modified

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,21 +1,66 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect, useContext} from 'react';
 import Row from './Row';
+import {ThemeContext,LocaleContext} from './context.js';
 
 export default function Greeting() {
- const [name, setName] = useState('Tom');
+ console.log("Greeting...");
+ const name = useFormInput('Tom')
+ const surName = useFormInput('Tom');
+ const theme = useContext(ThemeContext);
+ const locale = useContext(LocaleContext);
+ const width = useWindowsWidth();
 
- function handleNameChange(e) {
-   setName(e.target.value)
+ useDocumentTitle(name.value+ ' '+ surName.value, name.value);
+
+return (
+  <section className= {theme}>
+    <h1>* React Hook Demo *</h1>
+    <Row label="Name">
+      <input {...name} />
+    </Row>
+    <Row label="SurName">
+      <input {...surName} />
+    </Row>
+    <Row label="Width">
+      {width}
+    </Row>
+      <Row label="Language">
+      {locale}
+    </Row>
+  </section>
+);
+}
+
+
+function useFormInput(initalValue){
+  const [value, setValue] = useState(initalValue);
+  function handleSurnameChange(e) {
+    setValue(e.target.value)
+ } 
+ return {
+   value: value,
+   onChange: handleSurnameChange
  }
+}
 
-  return (
-    <section >
-      <h1>* React Hook Demo *</h1>
-      <Row label="Name">
-        <input value = {name} onChange={handleNameChange} />
-      </Row>
-    </section>
-  );
+function useDocumentTitle(title, dep){
+  useEffect(()=>{
+   console.log("useDocumentTitle ...");
+   document.title = title;
+  },[dep])
+}
 
+function useWindowsWidth(){
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(()=>{
+    const handleResize = ()=>{
+        setWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', handleResize);
+    return ()=>{
+      window.removeEventListener('resize', handleResize);
+    }
+  })
+  return width;
 }
 
